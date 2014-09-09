edb0e88f697d7d66acedd09329f671d6f8931b6d
George Mihailov <mihailoff@gmail.com>
2014-09-09 09:57:48 -0700

    WIP: Include more variants

--- conf/phpbrew_variants.sh
+++ conf/phpbrew_variants.sh
@@ -1,1 +1,1 @@
-PHPBREW_VARIANTS="+default +dbs +mb +iconv +gd=shared"
+PHPBREW_VARIANTS="+default +dbs +mb +iconv +openssl +xmlrpc +soap +intl +gd=shared -- --enable-gd-natf --with-jpeg-dir=/usr --with-png-dir=/usr"
